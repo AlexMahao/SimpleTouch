a210c720244878bb130912a5926f66daeef3ae42
mahao <mahao@huli.com>
2018-12-03 10:02:22 +0800

    修改no-op的Config方法

--- library-no-op/src/main/java/com/spearbothy/touch/core/Config.java
+++ library-no-op/src/main/java/com/spearbothy/touch/core/Config.java
@@ -5,5 +5,58 @@ package com.spearbothy.touch.core;
  */
 
 public class Config {
-    // 空实现
+    // 输出的日志以极简模式输出
+    private boolean isSimple = true;
+    // 是否延迟打印日志，延迟打印日志会在触摸事件结束之后打印，并且具有去重功能
+    private boolean isDelay = true;
+    // 是否保留重复的，默认不保留
+    private boolean isRepeat = false;
+    // 是否写入到文件
+    private boolean isPrint2File = true;
+    // 是否处理，不处理则不会监听任何方法，任何功能都无法生效
+    private boolean isProcess = true;
+
+    public boolean isSimple() {
+        return isSimple;
+    }
+
+    public Config setSimple(boolean simple) {
+        isSimple = simple;
+        return this;
+    }
+
+    public boolean isDelay() {
+        return isDelay;
+    }
+
+    public Config setDelay(boolean delay) {
+        isDelay = delay;
+        return this;
+    }
+
+    public boolean isRepeat() {
+        return isRepeat;
+    }
+
+    public Config setRepeat(boolean repeat) {
+        isRepeat = repeat;
+        return this;
+    }
+
+    public boolean isPrint2File() {
+        return isPrint2File;
+    }
+
+    public Config setPrint2File(boolean print2File) {
+        isPrint2File = print2File;
+        return this;
+    }
+
+    public boolean isProcess() {
+        return isProcess;
+    }
+
+    public void setProcess(boolean process) {
+        isProcess = process;
+    }
 }
